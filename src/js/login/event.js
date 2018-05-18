0be8454059bff25a6c9df34feb86d64f40eacad3
ljhhhhhh <13216698987@163.com>
2018-05-18 16:38:41 +0800

    '5-6'

---
+++ src/js/login/event.js
@@ -0,0 +1,20 @@
+export default (opts = {}) => {
+
+}
+
+
+// import formCheck from '../common/formCheck.js';
+
+// export default () => {
+//   const btn = document.getElementById('submit');
+//   const input = document.getElementById('input');
+//   const check = formCheck(document.getElementById('form'));
+//   console.log(check);
+//   btn.onclick = () => {
+//     check();
+//     alert(111);
+//   }
+//   input.oninput = () => {
+//     check();
+//   }
+// }
---
+++ src/js/login/init.js
@@ -0,0 +1,19 @@
+import '../common/polyfill.js';
+import render from './render.js';
+import bindEvent from './event.js';
+
+const login = (opts = {}) => {
+  const defaultOpts = {
+    loginBtnText: '登 录',
+    accountPlaceholder: '手机号/邮箱/账号',
+    passwordPlaceholder: '请填写密码',
+    accountLable: '',
+    passwordLable: ''
+  };
+
+  const options = Object.assign(defaultOpts,opts);
+  render(options);
+  bindEvent(options);
+}
+
+export { login }
